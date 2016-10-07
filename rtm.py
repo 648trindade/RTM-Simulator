c3cc520e98ee76ac91d07890fed9d8e92a6fc386
648trindade <rafaelgt.mbin@gmail.com>
2016-10-07 17:49:15 -0300

    mods

--- rtm.py
+++ rtm.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+"""Pacote com classes que implementam em conjunto um simulador de uma máquina
+de turing reversível"""
+
 class RTM:
+    """Classe representando uma máquina de turing reversível. Contém atributos
+    com objetos referentes a seus componentes"""
     def __init__(self):
         self.estados = {
             'A': [],
@@ -16,6 +21,14 @@ class RTM:
 class Estado:
     def __init__(self, nome):
         self.nome = nome
+        self.transicoes = []
+
+    def addTransicao(self, transicao):
+        self.transicoes.append(transicao)
+    
+    def temTransicao(self, transicao):
+        return transicao in self.transicoes
+
 
 class Fita:
     def __init__(self, conteudo):
@@ -42,4 +55,37 @@ class Fita:
         return self.conteudo
 
 class Transicao:
-    pass
+    def __init__(self, origem, fitas_in, destino, fitas_out):
+        self.origem = origem
+        self.fitas_in = fitas_in
+        self.destino = destino
+        self.fitas_out = fitas_out
+    
+    def __eq__(self, fita):
+        return self.fitas_in == fita
+    
+    def __getitem__(self, index):
+        return self.fitas_out[i]
+    
+
+class Cabecote:
+    def __init__(self):
+        self.pos = 0
+        self.fita = Fita("")
+
+    def moveRight(self):
+        self.pos += 1
+    
+    def moveLeft(self):
+        self.pos -= 1
+    
+    def read(self):
+        return self.fita.le(self.pos)
+    
+    def write(self, symbol):
+        self.fita.escreve(self, symbol)
+        if self.pos < 0 and symbol != 'B':
+            self.pos = 0
+
+"""uashdusahduashduashduashduashdu asudh asudhasu hasueh saueh uahe uasr hfuasrh asur fhauhriw uhfaiwurh iawu h"""
+
